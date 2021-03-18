2e338724f2f1da91ab02ae32ebe710f9e819a258
Connor Bowlan <connor@cbowlan.com>
2021-03-18 10:49:10 -0700

    This is going to commit Program2.cs

---
+++ A C Sharp Console App/Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace A_C_Sharp_Console_App
+{
+	class Program2
+	{
+		static void Main(string[] args)
+		{
+			double width, height, woodLength, glassArea;
+			string widthString, heightString;
+
+			// I added this to Master
+			string firstName, lastName;
+			Console.WriteLine("Enter your first name: ");
+			firstName = Console.ReadLine();
+
+			Console.WriteLine("Enter your first name: ");
+			lastName = Console.ReadLine();
+
+			Console.WriteLine("Enter the length of the wood: ");
+			widthString = Console.ReadLine();
+			width = double.Parse(widthString);
+
+			Console.WriteLine("Enter the length of the glass: ");
+			heightString = Console.ReadLine();
+			height = double.Parse(heightString);
+
+			woodLength = 2 * (width + height) * 3.25;
+			glassArea = 2 * (width * height);
+
+			Console.WriteLine("Client: " + firstName + " " + lastName);
+			Console.WriteLine("The length of the wood is " + woodLength + " feet");
+			Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+		}
+	}
+}
